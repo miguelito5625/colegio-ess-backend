e35bb36e07ad05e54c66dfe33f555b7c97dc9323
exbinario <miguel.4to.1994@gmail.com>
2022-08-18 21:46:37 +0000

    cambios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GradosModule } from './grados/grados.module';
 
 @Module({
-  imports: [GradosModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'admin',
+      password: 'Mariobross5625.',
+      database: 'colegio',
+      entities: [],
+      synchronize: true,
+    }),
+    GradosModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
